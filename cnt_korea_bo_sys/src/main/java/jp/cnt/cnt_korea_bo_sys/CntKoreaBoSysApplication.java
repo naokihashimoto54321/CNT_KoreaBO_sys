df40ce10fcf5f18e86efc585892201508245c0ab
naoki.hashimoto <naoki.hashimoto54321@gmail.com>
2021-12-14 00:39:52 +0900

    Initialize

---
+++ cnt_korea_bo_sys/src/main/java/jp/cnt/cnt_korea_bo_sys/CntKoreaBoSysApplication.java
@@ -0,0 +1,13 @@
+package jp.cnt.cnt_korea_bo_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CntKoreaBoSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CntKoreaBoSysApplication.class, args);
+	}
+
+}
---
+++ cnt_korea_bo_sys/src/test/java/jp/cnt/cnt_korea_bo_sys/CntKoreaBoSysApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.cnt.cnt_korea_bo_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CntKoreaBoSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
